e4593b22e5d304f7af0ae277dd02c38b0e329ff3
uiuqM <wilque.coelho@mail.uft.edu.br>
2024-12-19 15:58:46 -0300

    fix remove repo name from tag

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ color = os.environ.get('APP_COLOR')
 
 @app.route('/')
 def home():
-    return render_template('index.html', color="green")
+    return render_template('index.html', color="white")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
